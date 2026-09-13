9a386df1044ae3f931dde4f602e297c78175aba9
Mohd Talha <mawtalha5112004@gmail.com>
2026-09-13 12:38:46 +0530

    feat: get event by id

--- backend/src/controller/event.controller.ts
+++ backend/src/controller/event.controller.ts
@@ -22,3 +22,15 @@ export const GetAllEvents = async (req: Request, res: Response) => {
     return res.status(500).json({ message: 'Internal Server Error' })
   }
 }
+
+export const GetEventbyId = async (req: Request, res: Response) => {
+  try {
+    const eventId = req.params.id
+    const event = await (EventBase as any).findById(eventId)
+    console.log(event)
+    return res.json({ event })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
--- backend/src/routes/event.Route.ts
+++ backend/src/routes/event.Route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { NewEvent, GetAllEvents } from '../controller/event.controller.js'
+import { NewEvent, GetAllEvents, GetEventbyId } from '../controller/event.controller.js'
 
 const router = Router()
 
 router.post('/', NewEvent)
 router.get('/', GetAllEvents)
+router.get('/:id', GetEventbyId)
 
 export default router;
