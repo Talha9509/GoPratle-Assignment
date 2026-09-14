d848ab707dd63f9c9cbc8692f501a860639f6a33
Mohd Talha <mawtalha5112004@gmail.com>
2026-09-14 20:22:58 +0530

    chore: update deployement settings for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 await connectDB()
 
-const allowedOrigins = ['http://localhost:3000']
+const frontend = process.env.FRONTEND;
+const allowedOrigins = ['http://localhost:3000', frontend!]
 app.use(cors({
   origin: allowedOrigins
 }))
@@ -20,3 +21,11 @@ app.use('/api/events', EventRoutes)
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
+
+export default app;
